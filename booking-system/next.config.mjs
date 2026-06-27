8d59393e9a64464cef80d0cee746791426073689
Booking FMC <deploy@bookingfmc.com>
2026-06-27 12:33:33 +0100

    fix(deploy): optimize next.js and prisma for azure functions

--- booking-system/next.config.mjs
+++ booking-system/next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     typescript: {
         ignoreBuildErrors: true,
     },
 
     outputFileTracingIncludes: {
         '/api/**/*': ['./node_modules/@prisma/client/**/*', './prisma/schema.prisma'],
+    },
+    outputFileTracingExcludes: {
+        '*': [
+            'node_modules/@prisma/engines/**/*.node',
+            'node_modules/@prisma/client/libquery_engine-debian-openssl-3.0.x.so.node',
+            'node_modules/@prisma/client/query_engine-windows.exe.node',
+        ],
     },
     experimental: {
     },
